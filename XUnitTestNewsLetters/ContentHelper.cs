fc7288d1564a2f66481d3c7ccbb778bf3501ce20
Misael27 <mjpolidor@hotmail.com>
2023-03-29 02:15:20 -0400

    xunit tests

---
+++ XUnitTestNewsLetters/ContentHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitTestNewsLetters
+{
+    public static class ContentHelper
+    {
+        public static StringContent GetStringContent(object obj)
+            => new StringContent(JsonConvert.SerializeObject(obj), Encoding.Default, "application/json");
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static string RandomStringNumber(int length)
+        {
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static DateTime RandomDay()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
+        }
+
+        public static float RandomFloat()
+        {
+            double min = 1;
+            double max = 10;
+            double range = max - min;
+            double sample = random.NextDouble();
+            double scaled = (sample * range) + min;
+            return (float)scaled;
+        }
+    }
+}
---
+++ XUnitTestNewsLetters/NewsLetterTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestNewsLetters
+{
+    public class NewsLetterTests : IClassFixture<TestFixture<Program>>
+    {
+        private HttpClient Client;
+
+        public NewsLetterTests(TestFixture<Program> fixture)
+        {
+            Client = fixture.Client;
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyQGV4YW1wbGUuY29tIiwianRpIjoiOTY0NGM5MWYtNzlkYy00ODYxLTlmZmMtMWVlZTJjZTNlOWVkIiwiaWF0IjoxNjgwMDYxNTQ5LCJpZCI6IjJhZjcxODNiLTU4MzEtNDgwYy1iMjY2LTc0MDJlYjQ3NGJkNyIsIm5iZiI6MTY4MDA2MTU0OCwiZXhwIjoxNjg1MjQ1NTQ4LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUyMDIvIiwiYXVkIjoidGVzdCJ9.Y6UtX6HHNyzcWP09WIlvRUVVwUAZXeWgF2NSU2it1eQ");
+        }
+
+        [Fact]
+        public async Task WhenCreateNewsLetterReturnSucess()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "api/NewsLetter/upsert",
+                Body = new 
+                {
+                    Title = "Test title",
+                    HtmlBody = "<p>contenido</p>"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task WhenCreateNewsLetterWithErrorReturnBadRequest()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "api/NewsLetter/upsert",
+                Body = new
+                {
+                    Title = ""
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+    }
+}
